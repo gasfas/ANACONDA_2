34d8f071a833fdbb8e7bfe513dba06313b82eb89
nat12bbo <31130857+nat12bbo@users.noreply.github.com>
2017-11-09 18:03:58 +0100

    Updated md_edit open variables for GUI Plot tab
    Now also works if multiple experiments are selected.

--- package/+GUI/+plot/+md_edit/open_Variables.m
+++ package/+GUI/+plot/+md_edit/open_Variables.m
@@ -2,13 +2,14 @@ function open_Variables ()
 % This function opens up the variables reader to view the plotting
 % configuration.
 md_GUI = evalin('base', 'md_GUI');
-% fetch all the current names:
-exp_name = ['exp', num2str(md_GUI.plot.filenumber_selected)];
 plottype = char(md_GUI.UI.UIPlot.def.Popup_plot_type.String(md_GUI.UI.UIPlot.def.Popup_plot_type.Value));
 typesplit = strsplit(plottype, '.');
-det_name = ['det' num2str(find(strcmp(char(typesplit(1)), md_GUI.mdata_n.(exp_name).spec.det_modes)))];
 plottype = char(typesplit(2));
-confname = ['md_GUI.mdata_n.' exp_name '.plot.' det_name '.' plottype];
-
-openvar(confname)
+% fetch all the current names:
+for ll = 1:length(md_GUI.UI.UIPlot.LoadedFilesPlotting.Value)
+    exp_name(ll) = cellstr(['exp', num2str(md_GUI.UI.UIPlot.LoadedFilesPlotting.Value(ll))]);
+    det_name(ll) = cellstr(['det' num2str(find(strcmp(char(typesplit(1)), md_GUI.mdata_n.(char(exp_name(ll))).spec.det_modes)))]);
+    confname(ll) = cellstr(['md_GUI.mdata_n.' char(exp_name(ll)) '.plot.' char(det_name(ll)) '.' plottype]);
+    openvar(char(confname(ll)))
+end
 end
