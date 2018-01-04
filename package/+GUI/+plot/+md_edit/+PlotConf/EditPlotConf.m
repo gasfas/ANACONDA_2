8d954cd50c1031b939a9b9c464835d21b1dfed96
nat12bbo <31130857+nat12bbo@users.noreply.github.com>
2018-01-04 18:56:19 +0100

    Updated GUI
    Updated the EditPlotConf function and the callbacks for the set X- and Y- signal pointers (new plot conf tab). Added comments and descriptions for all create layout functions.

--- package/+GUI/+plot/+md_edit/+PlotConf/EditPlotConf.m
+++ package/+GUI/+plot/+md_edit/+PlotConf/EditPlotConf.m
@@ -2,8 +2,6 @@
 %   - inputs:
 %           Selected exp. number                (experiment_selected_number)
 %           Selected experiment(s) metadata     (mdata_n)
-%           Experiment settings                 (expsettings)
-%           Plot settings                       (plotsettings)
 %   - outputs: (into PlotConfSetButton)
 %           Field selected              (fieldselected)
 %           Field selected value      	(fieldselectedvalue)
@@ -19,7 +17,6 @@ exp_names = cellstr('');
 for lx = 1:length(filenumber)
     exp_names(lx) = cellstr(['exp', int2str(filenumber(lx))]);
 end
-selectedconf = cellstr('In_Workspace');
 for ly = 1:length(exp_names)
     current_exp_name = char(exp_names(lx));
     % Get number of detectors.
@@ -34,30 +31,15 @@ for ly = 1:length(exp_names)
         current_det_name = char(detector_choices(ly));
         detnr			 = IO.detname_2_detnr(current_det_name);
     end
-    detmodes = md_GUI.mdata_n.(char(exp_names(ly))).spec.det_modes;
-    detname = fieldnames(md_GUI.mdata_n.(char(exp_names(ly))).plot);
-    if strcmp(selectedconf, 'In_Workspace')
-        detname = char(detname(detnr));
-        confname(ly) = cellstr(['md_GUI.mdata_n.' char(exp_names(ly)) '.plot.user.' detname '.In_Workspace']);
-    else
-        selectedconf = strsplit(char(selected_confs(ly)), '.');
-        selectedconfplot = char(selectedconf(2));
-        selectedconfdetname = char(selectedconf(1));
-        for lz = 1:length(detmodes)
-            if strcmp(selectedconfdetname, char(detmodes(lz)))
-                detnum = lz;
-            end
-        end
-        detname = char(detname(detnum));
-        confname(ly) = cellstr(['md_GUI.mdata_n.' char(exp_names(ly)) '.plot.user.' detname '.' selectedconfplot]);
-    end
+    detname = ['det', num2str(detnr)];
+    confname(ly) = cellstr(['md_GUI.mdata_n.' char(exp_names(ly)) '.plot.user.' detname '.In_Workspace']);
     % Open up the variables reader to view and/or edit the plotting configuration.
     if length(confname) > 1
         openvar(char(confname(ly)))
     else
         openvar(char(confname))
     end
-    GUI.log.add(['User plot configuration ', selectedconfplot, ' for ', char(exp_names(ly)), ' opened in .']);
+    GUI.log.add(['The user plot configuration in workspace [ In_Workspace ] for ', char(exp_names(ly)), ' opened in .']);
 end
 assignin('base', 'md_GUI', md_GUI)
 end
